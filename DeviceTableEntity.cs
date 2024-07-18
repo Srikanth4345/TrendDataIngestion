ccffff8a87096800cc6d1b36b54b9b1a5647dd20
Phemanth85 <102746578+Phemanth85@users.noreply.github.com>
2024-07-18 04:22:56 +0000

    Add files via upload

---
+++ DeviceTableEntity.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Azure.Cosmos.Table;
+
+public class DeviceEntity : TableEntity
+{
+    public DeviceEntity(string deviceId, string deviceName)
+    {
+        PartitionKey = deviceId;
+        RowKey = deviceName;
+    }
+
+    public DeviceEntity() { }
+
+    public string DeviceId { get; set; }
+    public string DeviceName { get; set; }
+    public string DeviceProfileId { get; set; }
+}
